2711dda86744f5722f2c8303a73ae8b5ee404de4
MichaelG-B <beyer.gmichael@gmail.com>
2022-07-20 16:26:50 -0500

    queries and retirement csv

---  Schema.sql
+++  Schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE employees (
 
 -- Create tables for dept_manager
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -60,5 +60,22 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
+--Create Table for Dept_emp
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+
 SELECT * FROM departments;
-	
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
